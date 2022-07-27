b7f853ac01091c131744c67ddda16c58b0e737a5
Michał Świdziński <michalswidzinski01@gmail.com>
2022-07-27 14:44:53 +0200

    Add code that checks for existance of set of directories that will be used to sort files and create them if any is lacking, additonal minor code refactor

--- sorting.py
+++ sorting.py
@@ -2,8 +2,25 @@ import os
 import shutil
 
 
-def sorting():
-    path = input("Insert path: ")
+path = input("Insert path: ")
+
+
+def folders(path):
+    if not os.path.exists(f"{path}/Image"):
+        os.makedirs(f"{path}/Image")
+    if not os.path.exists(f"{path}/Execs"):
+        os.makedirs(f"{path}/Execs")
+    if not os.path.exists(f"{path}/Video"):
+        os.makedirs(f"{path}/Video")
+    if not os.path.exists(f"{path}/Text"):
+        os.makedirs(f"{path}/Text")
+    if not os.path.exists(f"{path}/Zips"):
+        os.makedirs(f"{path}/Zips")
+    if not os.path.exists(f"{path}/Others"):
+        os.makedirs(f"{path}/Others")
+
+
+def sorting(path):
     for file in os.listdir(path):
         if file.endswith(".jpg") or file.endswith(".png"):
             shutil.move(f"{path}/{file}", f"{path}/Image")
@@ -17,7 +34,8 @@ def sorting():
             shutil.move(f"{path}/{file}", f"{path}/Zips")
         else:
             if os.path.isfile(f"{path}/{file}"):
-                shutil.move(f"{path}/{file}", f"{path}/EE")
+                shutil.move(f"{path}/{file}", f"{path}/Others")
 
 
-sorting()
+folders(path)
+sorting(path)
