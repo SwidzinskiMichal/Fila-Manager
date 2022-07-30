bdb56c5c635a57bd03f2767ddecc4ebd46f58763
Michał Świdziński <michalswidzinski01@gmail.com>
2022-07-30 12:54:15 +0200

    Add function to delete empty directories as well as code refactoring

--- sorting.py
+++ sorting.py
@@ -1,41 +1,41 @@
 import os
 import shutil
 
-
-path = input("Insert path: ")
-
-
-def folders(path):
-    if not os.path.exists(f"{path}/Image"):
-        os.makedirs(f"{path}/Image")
-    if not os.path.exists(f"{path}/Execs"):
-        os.makedirs(f"{path}/Execs")
-    if not os.path.exists(f"{path}/Video"):
-        os.makedirs(f"{path}/Video")
-    if not os.path.exists(f"{path}/Text"):
-        os.makedirs(f"{path}/Text")
-    if not os.path.exists(f"{path}/Zips"):
-        os.makedirs(f"{path}/Zips")
-    if not os.path.exists(f"{path}/Others"):
-        os.makedirs(f"{path}/Others")
+path_to_sorting = input("Insert path: ")
 
 
-def sorting(path):
-    for file in os.listdir(path):
-        if file.endswith(".jpg") or file.endswith(".png"):
-            shutil.move(f"{path}/{file}", f"{path}/Image")
-        elif file.endswith(".exe"):
-            shutil.move(f"{path}/{file}", f"{path}/Execs")
-        elif file.endswith(".webm") or file.endswith(".mp4") or file.endswith(".mov"):
-            shutil.move(f"{path}/{file}", f"{path}/Video")
-        elif file.endswith(".odt") or file.endswith(".pdf"):
-            shutil.move(f"{path}/{file}", f"{path}/Text")
-        elif file.endswith(".rar") or file.endswith(".7z") or file.endswith(".zip"):
-            shutil.move(f"{path}/{file}", f"{path}/Zips")
+def clean_empty_directories(chosen_directory):
+    for directory in os.listdir(chosen_directory):
+        if len(os.listdir(f"{chosen_directory}/{directory}")) == 0:
+            os.rmdir(f"{chosen_directory}/{directory}")
         else:
-            if os.path.isfile(f"{path}/{file}"):
-                shutil.move(f"{path}/{file}", f"{path}/Others")
+            pass
 
 
-folders(path)
-sorting(path)
+def setup_directories(chosen_directory):
+    expected_directories = ["Others", "Zips", "Text", "Video", "Execs", "Image"]
+    for directory in expected_directories:
+        if not os.path.exists(f"{chosen_directory}/{directory}"):
+            os.makedirs(f"{chosen_directory}/{directory}")
+
+
+def sort_files(chosen_directory):
+    for file in os.listdir(chosen_directory):
+        if file.endswith((".jpg", ".png", ".jpeg", ".gif", ".webp")):
+            shutil.move(f"{chosen_directory}/{file}", f"{chosen_directory}/Image")
+        elif file.endswith(".exe"):
+            shutil.move(f"{chosen_directory}/{file}", f"{chosen_directory}/Execs")
+        elif file.endswith((".webm", ".mp4", ".mov")):
+            shutil.move(f"{chosen_directory}/{file}", f"{chosen_directory}/Video")
+        elif file.endswith((".odt", ".pdf", ".txt")):
+            shutil.move(f"{chosen_directory}/{file}", f"{chosen_directory}/Text")
+        elif file.endswith((".rar", ".7z", ".zip")):
+            shutil.move(f"{chosen_directory}/{file}", f"{chosen_directory}/Zips")
+        else:
+            if os.path.isfile(f"{chosen_directory}/{file}"):
+                shutil.move(f"{chosen_directory}/{file}", f"{chosen_directory}/Others")
+
+
+setup_directories(path_to_sorting)
+sort_files(path_to_sorting)
+clean_empty_directories(path_to_sorting)
