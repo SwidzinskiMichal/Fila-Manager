3d3fda2d377746d16582fe8b4f5d31185f5c63cd
Michał Świdziński <michalswidzinski01@gmail.com>
2022-09-09 18:48:24 +0200

    Add schema.json and test to validate, add new options for cli.py

--- cli.py
+++ cli.py
@@ -1,14 +1,26 @@
 import click
-from sorting import remove_empty_directories
+from sorting import remove_created_empty_directories, remove_empty_directories, sort_files, setup_directories
 
 
 @click.command()
-@click.option('--remove-empty-dirs', is_flag=True, help='Remove empty directories')
 @click.option('--path', default=".", help='Provide path for sorting')
-def run(path, remove_empty_dirs):
+@click.option('--remove-empty-dirs', is_flag=True, help='Remove empty directories')
+@click.option('--remove-created-empty-dirs', is_flag=True, help='Remove empty directories created by setup-dirs')
+@click.option('--setup-dirs', , help='Setup directories needed for sorting')
+@click.option('--sort', help ='Sort files in current directory or one provided as path')
+def run(path, sort, remove_empty_dirs, remove_created_empty_dirs, setup_dirs):
     if remove_empty_dirs:
         print("Removing empty directories...")
         remove_empty_directories(path)
+    if remove_created_empty_dirs:
+        print("Removing empty directories...")
+        remove_created_empty_directories(path)
+    if setup_dirs:
+        print("Setting up directories...")
+        setup_directories(path)
+    if sort:
+        print("Sorting...")
+        sort_files(path)
 
 if __name__ == '__main__':
     run()
--- sorting.py
+++ sorting.py
@@ -5,7 +5,8 @@ from pathlib import Path
 
 
 with open("config.json") as config_dict:
-    file_categories = json.load(config_dict)
+    config = json.load(config_dict)
+    file_categories = config["file_categories"]
 
 
 def sort_files(chosen_directory):
@@ -20,6 +21,10 @@ def remove_empty_directories(chosen_directory):
         if os.path.isdir(f"{chosen_directory}/{name}") and len(os.listdir(f"{chosen_directory}/{name}")) == 0:
             os.rmdir(f"{chosen_directory}/{name}")
 
+def remove_created_empty_directories(chosen_directory):
+    for name in os.listdir(chosen_directory):
+        if os.path.isdir(f"{chosen_directory}/{name}") and len(os.listdir(f"{chosen_directory}/{name}")) == 0 and name in file_categories:
+            os.rmdir(f"{chosen_directory}/{name}")
 
 def setup_directories(chosen_directory):
     expected_directories = list(file_categories.keys())    
