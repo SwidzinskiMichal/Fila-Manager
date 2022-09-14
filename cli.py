3c61c475848c59ff55dbac26a5a7f9c40a4725e4
Michał Świdziński <michalswidzinski01@gmail.com>
2022-09-14 18:42:22 +0200

    Add test for removing only empty directories that were created by based on config.json

--- cli.py
+++ cli.py
@@ -6,7 +6,7 @@ from sorting import remove_created_empty_directories, remove_empty_directories, 
 @click.option('--path', default=".", help='Provide path for sorting')
 @click.option('--remove-empty-dirs', is_flag=True, help='Remove empty directories')
 @click.option('--remove-created-empty-dirs', is_flag=True, help='Remove empty directories created by setup-dirs')
-@click.option('--setup-dirs', , help='Setup directories needed for sorting')
+@click.option('--setup-dirs', help='Setup directories needed for sorting')
 @click.option('--sort', help ='Sort files in current directory or one provided as path')
 def run(path, sort, remove_empty_dirs, remove_created_empty_dirs, setup_dirs):
     if remove_empty_dirs:
