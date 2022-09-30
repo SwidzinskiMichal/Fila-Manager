57eaaf7e1b70462c0849e6f626936b2d899bb05d
Michał Świdziński <michalswidzinski01@gmail.com>
2022-09-30 17:49:27 +0200

    Add defaults for options loaded from config.json

--- cli.py
+++ cli.py
@@ -15,8 +15,8 @@ with open("config.json") as config_dict:
 
 @click.command()
 @click.option('--path', default=".", help='Provide path for sorting')
-@click.option('--remove-empty-dirs', is_flag=cli_config["remove_created_empty_dirs"], help='Remove empty directories')
-@click.option('--remove-created-empty-dirs', is_flag=cli_config["remove_empty_dirs"], help='Remove empty directories created by setup-dirs')
+@click.option('--remove-empty-dirs', is_flag=True, default=cli_config["remove_empty_dirs"], help='Remove empty directories')
+@click.option('--remove-created-empty-dirs', is_flag=True, default=cli_config["remove_created_empty_dirs"], help='Remove empty directories created by setup-dirs')
 @click.option('--setup-dirs', is_flag=True, help='Setup directories needed for sorting')
 @click.option('--sort', is_flag=True, help ='Sort files in current directory or one provided as path')
 def run(path, sort, remove_empty_dirs, remove_created_empty_dirs, setup_dirs):
@@ -24,7 +24,7 @@ def run(path, sort, remove_empty_dirs, remove_created_empty_dirs, setup_dirs):
         print("Removing empty directories...")
         remove_empty_directories(path)
     if remove_created_empty_dirs:
-        print("Removing empty directories...")
+        print("Removing created empty directories...")
         remove_created_empty_directories(path)
     if setup_dirs:
         print("Setting up directories...")
